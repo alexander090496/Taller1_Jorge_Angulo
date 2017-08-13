db3a0e698f813e66da333b58629cf75a8752b0f1
Alexander <jorgeAn09@hotmail.com>
2017-08-13 13:34:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alex/taller1_jorge_angulo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex.taller1_jorge_angulo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
